b7ab3e316ba5876931779b44959b2834bc5e45f4
Merve Şahinoğlu <artemiswolfs@gmail.com>
2023-10-23 14:56:07 +0300

    show spinner janras

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useData from "../hooks/useData";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "./../services/image-url";
@@ -6,7 +6,8 @@ import getCroppedImageUrl from "./../services/image-url";
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
   // const { data, error, isLoading } = useData<Genre>("genres");
-
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {data.map((x) => (
